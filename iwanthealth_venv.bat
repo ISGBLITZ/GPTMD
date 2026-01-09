84561db393170fb0d8995798644de8184d494bbf
IvanLightning2099 <ishaan.subin2013@gmail.com>
2026-01-08 18:44:00 -0800

    Update iwanthealth_venv.bat

--- iwanthealth_venv.bat
+++ iwanthealth_venv.bat
@@ -1,11 +1,11 @@
 @echo off
-REM --- Activate virtual environment ---
-call "C:\Users\Ishaa\OneDrive\Iwanthealth\IwantHealth_venv\Scripts\activate.bat"
 
 REM --- Set environment variables for AI Foundry ---
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 REM --- Launch Streamlit app ---
-streamlit run "C:\Users\Ishaa\OneDrive\Iwanthealth\streamlit_app.py" --server.port 8501
+REM --- Put wherever the location of your streamlit_app.py is ---
+streamlit run _____ --server.port 8501
 
 pause
+
